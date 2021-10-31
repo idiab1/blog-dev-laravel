0d6aeac15be0908890b2b22c0c7bf4403e1dcb5a
islamdiab-stack <islamdiab967@gmail.com>
2021-10-31 07:25:22 +0200

    redirect welcome route to home route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/',"")
+
+Route::redirect("/", "/home");
 
 Auth::routes();
 
