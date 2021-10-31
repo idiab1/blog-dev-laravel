d30e5ee44f95ea35109ce5be73ec20180182511b
islamdiab-stack <islamdiab967@gmail.com>
2021-10-31 09:40:34 +0200

    create new base for admin routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -43,9 +43,15 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
+            // Users Route
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            // Admins Routes
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/admin/web.php'));
         });
     }
 
