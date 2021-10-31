64c4a1ba49e1754c1d180f23109b59e843b921cb
islamdiab-stack <islamdiab967@gmail.com>
2021-10-31 10:23:05 +0200

    Validate on email, and password and check if user authanticated has value 1 for is_admin

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -37,4 +38,32 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function login(Request $request)
+    {
+        $input = $request->all();
+
+        $this->validate($request, [
+            "email" => "required|email",
+            "password" => "required",
+        ]);
+
+        if(auth()->attempt(array('email' => $input['email'], 'password' => $input['password']))){
+
+            if(auth()->user()->is_admin == 1){
+
+                return redirect()->route("admin.home");
+
+            }else{
+
+                return redirect()->route("home");
+
+            }
+
+        }else{
+            return redirect()->route("login")->with("error", "Email-address or Password is Wrong.");
+        }
+
+    }
+
 }
