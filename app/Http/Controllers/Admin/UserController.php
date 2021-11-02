8f96264756b6a461f53bf203cd1bfa02e417e962
islamdiab-stack <islamdiab967@gmail.com>
2021-11-02 10:29:42 +0200

    type create method for return create user page

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -26,7 +27,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        return view("admin.users.create");
     }
 
     /**
